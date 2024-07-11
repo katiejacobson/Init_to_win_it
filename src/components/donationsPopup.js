e8a3933940f801eab124a189cae116431fe58d6e
Imani Walker <imaniwalker0305@gmail.com>
2024-07-11 17:56:09 -0400

    I've started to work on the donation popup modal

--- src/components/donationsPopup.js
+++ src/components/donationsPopup.js
@@ -4,8 +4,27 @@ export default class donationsPopup extends popup{
     super({popupSelector});
     this._donationForm = this._popupElement.querySelector(".donation_form")
     this._formSubmit = formSubmit
-    this._submitButton = this._donationForm.querySelector('donate_button')
+    this._submitButton = this._donationForm.querySelector('.donate_button')
 }
-_getInputValues(
-})
+_getInputValues(){
+    const inputValues={};
+    const inputElements=this._donationForm.querySelectorAll('.donation_inputs');
+    inputElements.forEach((input)=>{
+        inputValues[input.name] = inputValues;
+    });
+    return inputValues
+}
+close(){
+    this._popupForm.reset()
+    super.close()
+}
+setEventListeners(){
+    this._donationForm.addEventListener('submit',(evt)=>{
+        evt.preventDefault();
+       
+        const inputValues = this._getInputValues 
+        this._formSubmit(inputValues);
+      })
+      super.setEventListeners();
+}
 }
