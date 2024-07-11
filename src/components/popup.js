33d69afb3db48b951d50c807095ec9a261aaaa57
Bree Massingill <breemassingill@gmail.com>
2024-07-11 18:02:40 -0500

    connect form submit to card render for DonationsPopup class

--- src/components/popup.js
+++ src/components/popup.js
@@ -1,30 +1,31 @@
-export default class popup{
-constructor({popup}){
+export default class popup {
+  constructor({ popupSelector }) {
     this._popupElement = document.querySelector(popupSelector);
-    this._popupCloseButton = this._popupElement.querySelector('close_popup')
-}
+    this._popupCloseButton = this._popupElement.querySelector("close_popup");
+  }
 
+  open() {
+    this._popupElement.classList.add("open_popup");
+    document.addEventListener("keydown", this._closeByEscape);
+  }
+  close() {
+    this._popupElement.classList.add("open_popup");
+    document.addEventListener("keydown", this._closeByEscape);
+  }
 
-open(){
-    this._popupElement.classList.add('open_popup');
-    document.addEventListener('keydown', this._closeByEscape )
-}
-close(){
-    this._popupElement.classList.add('open_popup');
-    document.addEventListener('keydown', this._closeByEscape)
-}
-
-_closeByEscape=(evt)=>{
-    if (evt.key==="Escape"){
-        this.close()
+  _closeByEscape = (evt) => {
+    if (evt.key === "Escape") {
+      this.close();
     }
+  };
+  setEventListeners() {
+    this._popupElement.addEventListener("click", (evt) => {
+      if (
+        evt.target === this._popupCloseButton ||
+        evt.target === this._popupElement
+      ) {
+        this.close();
+      }
+    });
+  }
 }
-setEventListeners(){
-    this._popupElement.addEventListener("click",(evt)=>{
-        if(evt.target === this._popupCloseButton || evt.target=== this._popupElement){
-            this.close();
-        }
-    })
-}
-}
- 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
+// IMPORTS
 import Card from "../components/Card.js";
 import Section from "../components/Section.js";
-import Api from "../components/Api.js";
-import Charities from "../components/Charities.js";
-import Donations from "../components/Donations.js";
+// import Api from "../components/Api.js";
+import DonationsPopup from "../components/DonationsPopup.js";
 import "../pages/index.css";
 
+// CLASS INSTANTATION
 const cardTemplate =
   document.querySelector("#card__template").content.firstElementChild;
 const createCard = (cardData) => {
@@ -14,8 +15,14 @@ const createCard = (cardData) => {
 const whyRenderer = (cardData) => {
   sectionWhy.addItem(createCard(cardData));
 };
-const donationRenderer = (cardData) => {
-  sectionDonations.addItem(createCard(cardData));
+const donationRenderer = (inputData) => {
+  sectionDonations.addItem(createCard(inputData));
 };
+const donationsPopup = new DonationsPopup(".donation__popup", donationRenderer);
 const sectionWhy = new Section(whyRenderer, ".why__gallery");
 const sectionDonations = new Section(donationRenderer, ".donations__gallery");
+
+// FORMS
+const donateForm = document.querySelector(".donation__form");
+
+// FUNCTIONS
