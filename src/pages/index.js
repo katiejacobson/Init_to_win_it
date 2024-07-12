9499401ddcc2dfd3a856f1a4773f27159b4259fa
Katie Jacobson <katieabole@gmail.com>
2024-07-12 08:28:54 -0400

    feature/charitycards

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,50 +2,44 @@
 import Card from "../components/Card.js";
 import Section from "../components/Section.js";
 
-
 import Donations from "../components/Donations.js";
 import "../pages/index.css";
 
-
-import Api from "../components/api.js";
-import Charities from "../components/charities.js";
+import Api from "../components/Api2.js";
+import Charities from "../components/Charities2.js";
 import CharityCard from "../components/CharityCard.js";
 
 import DonationsPopup from "../components/DonationsPopup.js";
-
-
-
-// const api = new Api({
-//   baseUrl: "https://api.charityapi.org",
-//   headers: {
-//     apikey:
-//       "live-Om_n2JQF00pe75rH-_Z0dxSyK_zUleLGuvZNju1Y9OzPQNiw2cj-PNd53keBX6i83oC34ZqR49aZssx7",
-//     // "Content-Type": "application/json",
-//   },
-// });
 
 const cardTemplate =
   document.querySelector("#card__template").content.firstElementChild;
 const donateButton = document.querySelector(".donate__button");
 
+//API instantiation
 const api = new Api({
   baseUrl: "https://partners.every.org/v0.2/",
   apiKey: "pk_live_51295acbffe33d27ac313b33feb97d63",
 });
 
-// const charityForm = new Charities(".charities", handleCharityFormSubmit);
-// charityForm.setEventListeners();
+const charityForm = new Charities(".charities", handleCharityFormSubmit);
+charityForm.setEventListeners();
 
 function handleCharityFormSubmit(data) {
   api
     .getInfo(data)
     .then((res) => {
       createCharityCard(res.nonprofits[2]);
+      charityCardList.renderItems(res.nonprofits);
     })
     .catch(console.error);
 }
 
+function createCharityCard(data) {
+  const charityCard = new CharityCard(data, "#charity-card-template");
+  return charityCard.generateCard();
+}
 
+const charityCardList = new Section(createCharityCard, "#charities-container");
 
 // CLASS INSTANTATION
 
@@ -73,4 +67,3 @@ donationsPopup.setEventListeners();
 
 // EVENT LISTENERS
 donateButton.addEventListener("click", donateButtonHandler);
-
